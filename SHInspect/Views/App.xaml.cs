4f2f1e3c28f537f15d3217278631746e387522e1
Tom Austin <tom@tomaustin.xyz>
2022-10-30 14:49:57 +0000

    Added logic to pull latest update from github releases.

--- SHInspect/Views/App.xaml.cs
+++ SHInspect/Views/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using SHInspect.ViewModels;
 using SHInspect.Views;
+using Squirrel;
+using Squirrel.Sources;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -35,6 +37,47 @@ namespace SHInspect
             {
                 e.Handled = true;
             }
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SquirrelAwareApp.HandleEvents(
+       onInitialInstall: OnAppInstall,
+       onAppUninstall: OnAppUninstall,
+       onEveryRun: OnAppRun);
+            await CheckForUpdates();
+        }
+
+        private static void OnAppInstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+        {
+            tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+        }
+
+        private static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+        {
+            tools.SetProcessAppUserModelId();
+        }
+
+        private static async Task CheckForUpdates()
+        {
+            try
+            {
+                using var mgr = new UpdateManager(new GithubSource("https://github.com/Streets-Heaver/SHInspect", "", false));
+                await mgr.UpdateApp();
+
+            }
+            catch (InvalidOperationException)
+            {
+                //Error with squirrel update, carry on...
+            }
+
+
         }
     }
 }
